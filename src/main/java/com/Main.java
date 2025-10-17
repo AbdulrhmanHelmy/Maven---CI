c2d8749113dbd842a9669c1cff6243cfe7073d47
HelMy711 <abdulllrhmanhelmy@gmail.com>
2025-10-17 22:47:17 +0300

    Add Maven project with GitHub Actions CI workflow test

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,16 +2,8 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Helmy, CI is running!");
+        System.out.println("Hello Helmy");
     }
 
-    public static int add(int a, int b) {
-        return a + b;
-    }
 
-    // Method that sometimes fails intentionally (based on current second)
-    public static boolean sometimesFail() {
-        int second = java.time.LocalTime.now().getSecond();
-        return second % 2 == 0;
-    }
 }
